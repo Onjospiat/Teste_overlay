3bdae469a6b09dfdd675b222a607bb28938bcb5f
Onjospiat <guilherme.chini.gani@gmail.com>
2025-03-15 19:23:24 -0300

    Teste de grid para a aula18

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello!');
+function func_overlay(elemento,i){
+    const text_over = document.getElementsByClassName("overlay");
+    text_over[i].style.width = elemento.offsetWidth + "px";
+    text_over[i].style.height = elemento.offsetHeight + "px";
+}
+
+window.func_overlay = func_overlay;
